92da0231de3bd73e4e6861a4e1dce55a14b22178
nmaster <dominik.renzel@gmail.com>
2017-01-16 15:43:21 +0100

    solved federated setup

--- fed/run.sh
+++ fed/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sed -i "s#9012#9011#g;s#134.61.172.204:9011#${FED_BOOTSTRAP}#g" ./bin/connect_network.sh
-cat ./bin/connect_network.sh
 
-#./bin/connect_network.sh
-./bin/start_network.sh
+#chmod 777 ./pastry.properties
+#dos2unix ./pastry.properties
+
+#cat ./bin/start_network.sh
+./bin/connect_network.sh
+#./bin/start_network.sh
