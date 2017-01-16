beb7331cd86eebb7d691ffa708d9ca5a8ab191d4
nmaster <dominik.renzel@gmail.com>
2017-01-16 15:58:27 +0100

    continued federation scenario

--- fed/run.sh
+++ fed/run.sh
@@ -2,9 +2,4 @@
 
 sed -i "s#9012#9011#g;s#134.61.172.204:9011#${FED_BOOTSTRAP}#g" ./bin/connect_network.sh
 
-#chmod 777 ./pastry.properties
-#dos2unix ./pastry.properties
-
-#cat ./bin/start_network.sh
 ./bin/connect_network.sh
-#./bin/start_network.sh
